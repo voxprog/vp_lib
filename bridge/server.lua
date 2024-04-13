1bd38d8995beed7ff4a9825947c821f71fbf74fb
boppe <163592761+voxprog@users.noreply.github.com>
2024-04-12 20:56:13 -0500

    Fix loading qb server bridge

--- bridge/server.lua
+++ bridge/server.lua
@@ -2,6 +2,6 @@ local using_qb = (GetResourceState('qb-core') ~= 'missing' or GetResourceState('
 local using_esx_legacy = GetResourceState('es_extended') ~= 'missing'
 
 return
-    (using_qb and require 'bridge.qb.client') or
+    (using_qb and require 'bridge.qb.server') or
     (using_esx_legacy and require 'bridge.esx.server') or
     (require 'bridge.custom.server')
